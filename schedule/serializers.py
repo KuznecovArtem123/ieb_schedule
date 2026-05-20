17e9bfae13147c247d74cf0a9b075cb4c15a786b
KuznecovArtem123 <newrebookdell@gmail.com>
2026-05-20 20:21:30 +0300

    fix serializers.py

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -20,5 +20,5 @@ class GroupSerializer(serializers.ModelSerializer):
 
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Lesson
+        model = Teacher
         fields = '__all__'
