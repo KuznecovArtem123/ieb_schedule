ce6ca382206cebfdb716ee039c57cb4bd3af0982
KuznecovArtem123 <newrebookdell@gmail.com>
2026-09-23 23:38:50 +0300

    backend etag realization

--- backend/schedule/tests.py
+++ backend/schedule/tests.py
@@ -1,4 +1,3 @@
-
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from rest_framework.test import APIClient
@@ -6,23 +5,26 @@ from rest_framework.test import APIClient
 from ieb_admin.models import Schedule
 
 
-class ScheduleVersionViewTests(TestCase):
+class ScheduleETagTests(TestCase):
 	def setUp(self):
 		self.client = APIClient()
-		self.schedule = Schedule.objects.create(
+		Schedule.objects.create(
 			edu=Schedule.Edu.SPO,
 			week=Schedule.Week.THIS,
 			file=SimpleUploadedFile('schedule.xlsx', b'schedule'),
 		)
 
-	def test_returns_schedule_id_and_version(self):
-		response = self.client.get('/api/schedule/version/?edu=spo&week=this')
+	def test_returns_not_modified_for_matching_etag(self):
+		response = self.client.get('/api/lessons/?edu=spo&week=this')
 
 		self.assertEqual(response.status_code, 200)
-		self.assertEqual(response.data['id'], self.schedule.id)
-		self.assertEqual(response.data['version'], self.schedule.updated_at.isoformat().replace('+00:00', 'Z'))
+		self.assertTrue(response['ETag'])
 
-	def test_returns_not_found_for_missing_schedule(self):
-		response = self.client.get('/api/schedule/version/?edu=vo&week=next')
+		cached_response = self.client.get(
+			'/api/lessons/?edu=spo&week=this',
+			HTTP_IF_NONE_MATCH=response['ETag'],
+		)
 
-		self.assertEqual(response.status_code, 404)
+		self.assertEqual(cached_response.status_code, 304)
+		self.assertEqual(cached_response['ETag'], response['ETag'])
+
--- backend/schedule/urls.py
+++ backend/schedule/urls.py
@@ -4,7 +4,6 @@ from .views import (
     GroupLessonsView,
     GroupView,
     LessonView,
-    ScheduleVersionView,
     TeacherLessonsView,
     TeacherView,
 )
@@ -13,7 +12,6 @@ urlpatterns = [
     path('lessons/fromGroup/<int:id>', GroupLessonsView.as_view(), name='api_lessonsFromGroup'),
     path('lessons/fromTeacher/<int:id>', TeacherLessonsView.as_view(), name='api_lessonsFromTeacher'),
     path('lessons/', LessonView.as_view(), name='api_lessons'),
-    path('schedule/version/', ScheduleVersionView.as_view(), name='api_schedule_version'),
     path('groups/', GroupView.as_view(), name='api_teachers'),
     path('teachers/', TeacherView.as_view(), name='api_groups'),
 ]
